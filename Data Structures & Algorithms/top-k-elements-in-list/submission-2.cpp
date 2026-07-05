584d00bc92097769e5f1dccee6e0476dda324797
Pranavvmore12 <141485313+Pranavvmore12@users.noreply.github.com>
2026-07-05 20:04:22 +0530

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    static bool cmp(const pair<int,int>&a, const pair<int,int>&b){
+       return a.second<b.second;
+    }
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+       unordered_map<int,int>hash;
+       for(int i=0;i<nums.size();i++){
+            hash[nums[i]]++;
+       }
+       vector<pair<int,int>>hashAns(hash.begin(),hash.end());
+    sort(hashAns.begin(),hashAns.end(),cmp);
+    
+    vector<int>ans;
+    for(int i=hashAns.size()-k;i<hashAns.size();i++){
+        ans.push_back(hashAns[i].first);
+    }
+     return ans;
+
+    }
+};
